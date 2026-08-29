2fa85f86a7806afc29685392cc45b4c07dd2a430
gyeon <jgh030814@naver.com>
2026-08-29 23:07:54 +0900

    ♻️ Refactor: Node 20.x 환경 호환성을 위해 jsdom을 happy-dom으로 대체 구현

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
     },
   },
   test: {
-    environment: 'jsdom',
+    environment: 'happy-dom',
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'html'],
